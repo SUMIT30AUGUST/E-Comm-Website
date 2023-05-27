ff3a4045a25e8f1c6fca266442b29d3312d60d01
sumitrawat30aug@gmail.com <33266129+SUMIT30AUGUST@users.noreply.github.com>
2023-05-27 14:06:49 +0530

    Product Detail Page setup

--- client/src/components/details/ActionItem.js
+++ client/src/components/details/ActionItem.js
@@ -1,12 +1,32 @@
 
-import { Box,Button } from "@mui/material"
+import { Box,Button ,styled} from "@mui/material"
+import ShoppingCartIcon  from '@mui/icons-material/ShoppingCart';
+import FlashOnIcon  from '@mui/icons-material/FlashOn';
+
+const LeftContainer = styled(Box)`
+ min-width: 40%;
+ padding: 40px 0 0 80px;
+`
+
+const Image = styled('img')({
+    padding: '15px 20px',
+    border: '1px solid #f0f0f0' ,
+   width:'95%'
+  })
+
+const StyledButton= styled(Button)`
+  width: 48%;
+  height: 50px;
+  border-radius: 2px;
+`
 
 const ActionItem = (product) => {
   return (
-     <Box>
-         <img src={product.url} alt="imggge"/>
-         
-     </Box>
+     <LeftContainer>
+         <Image src={product.product.url} alt="imggge"/>
+        <StyledButton variant="contained" style={{ marginRight: 10 , background: '#ff9f00'}}><ShoppingCartIcon/>Add to Cart</StyledButton>
+        <StyledButton variant="contained" style={{background:'#fb541b'}}><FlashOnIcon/>Buy now</StyledButton>
+     </LeftContainer>
   )
 }
 
--- client/src/components/details/DetailView.js
+++ client/src/components/details/DetailView.js
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
 import { useDispatch , useSelector } from "react-redux";
-//useSelector is used for getting values from the redux
+
 
 import ActionItem from "./ActionItem";
 import { useParams } from "react-router-dom";
-//useParams is a custom hook of react router dom
-// it is used to take out values from the URL
-//useParams is an object.
+
 
 import { getProductDetails } from "../../redux/actions/productAction";
 
-import {Box, Typography} from '@mui/material'
+import {Box, Typography,Grid,styled} from '@mui/material'
+
+const Component =styled(Box)`
+   background: #f2f2f2 ;
+   margin-top:55px ;
+`
+
+
+const Container = styled(Grid)`
+    background: #ffffff;
+    display: flex;
+`
+const RightContainer = styled(Grid)`
+   margin-top: 50px;
+   padding-left: 45px;
+`
 
 const DetailView = () => {
   
@@ -24,24 +37,31 @@ const DetailView = () => {
       dispatch(getProductDetails(id))
  },[dispatch,id,loading,product])
  
- console.log(product);
+
 
 
   return (
     <div>
-      <Box>
+      <Component>
             {
                   product && Object.keys(product).length &&
-                  <Box>
-                      <Box>
-                          <ActionItem product={product}/>
-                      </Box>
-                      <Box>
+                  <Container container>
+                      <Grid item lg={4} md={4} sm={8} xs={12}>
+                          <ActionItem product={product}/>                       
+                       </Grid>
+                      
+                      <RightContainer  item lg={8} md={8} sm={8} xs={12}>
                         <Typography>{product.title.longTitle}</Typography>
-                      </Box>
-                  </Box>
+                        <Typography style={{margin: 5, color:'#878787'}}>8 Ratings & 1 Review </Typography>
+                        <Typography>
+                          <Box component="span" style={{fontSize:28}}>₹{product.price.cost}</Box>&nbsp;
+                          <Box component="span" style={{color:'#878787'}}><strike>₹{product.price.mrp}</strike></Box>&nbsp;  
+                          <Box component="span" style={{color:'dark-green'}}>{product.price.discount} off</Box>  
+                        </Typography>
+                      </RightContainer>
+                  </Container>
             }
-      </Box>
+      </Component>
     </div>
   )
 }
